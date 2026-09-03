9efc2f95a97b68d47012a5340ce56e6eec91d42c
dylan <dylanub893@gmail.com>
2026-09-03 15:19:13 -0400

    feat: configure database context

---
+++ Rag.API/Data/Configuration/DocumentChunkConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rag.API.Domain;
+
+namespace Rag.API.Data.Configuration;
+
+public sealed class DocumentChunkConfiguration : IEntityTypeConfiguration<DocumentChunk>
+{
+    public void Configure(EntityTypeBuilder<DocumentChunk> builder) 
+    {
+        builder
+            .HasKey(dch => dch.Id);
+
+        builder
+            .Property(dch => dch.TextContent)
+            .IsRequired();
+
+        builder
+            .Property(dch => dch.Embedding)
+            .HasColumnType("vector(1536)");
+
+        builder
+            .Property(dch => dch.PageNumber)
+            .IsRequired();
+
+        builder.HasIndex(c => c.Embedding)
+            .HasMethod("hnsw")
+            .HasOperators("vector_cosine_ops");
+    }
+}
