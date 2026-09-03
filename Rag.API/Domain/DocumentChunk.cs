3de6f7bd056e461373e7f3d3ca83be96d0f97579
dylan <dylanub893@gmail.com>
2026-09-02 20:59:02 -0400

    feat: add Document and DocumentChunk entities

---
+++ Rag.API/Domain/DocumentChunk.cs
@@ -0,0 +1,12 @@
+﻿using Pgvector;
+
+namespace Rag.API.Domain;
+public sealed class DocumentChunk
+{
+    public Guid Id { get; set; }
+    public string TextContent { get; set; } = string.Empty;
+    public required Vector Embedding { get; set; }
+    public int PageNumber { get; set; }
+    public Guid DocumentId { get; set; }
+    public Document Document { get; set; } = null!;
+}
